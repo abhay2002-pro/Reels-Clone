9e2d5cb1f57deadd59d34d328a027f43ea1426a2
Abhay Kumar Ray <abhayray2002@gmail.com>
2022-07-19 00:14:24 +0530

    UI os sign up page added

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import styles from "./Signup.module.css";
+import insta from "../Assets/Instagram.JPG";
+import { Button, CardActions } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 
-function Signup() {
+export default function MultiActionAreaCard() {
+  const useStyles = makeStyles({
+    text1:{
+      color: 'grey',
+      textAlign: 'centre'
+    }
+  })
+  const classes = useStyles();
   return (
-    <div>Signup</div>
-  )
+    <div className= {styles.signupWrapper}>
+      <div className = {styles.signupCard}>
+      <Card variant="outlined">
+          <div className= {styles.instaLogo}>
+            <img src={insta} alt="Instagram" />
+          </div>
+          <CardContent>
+            <Typography variant="subtitle1" className={classes.text1}>
+              Sign up to see photos and videos from friends.
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Lizards are a widespread group of squamate reptiles, with over
+              6,000 species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+        <CardActions>
+          <Button size="small" color="primary">
+            Share
+          </Button>
+        </CardActions>
+      </Card>
+      </div>
+    </div>
+  );
 }
-
-export default Signup
