a0be7e6e5beec2aab34145c635e69ac3246e1fd7
Abhay Kumar Ray <abhayray2002@gmail.com>
2022-07-19 21:22:14 +0530

    applied routing, added login page, updated signup page

---
+++ src/Components/Login.js
@@ -0,0 +1,133 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import styles from "./Login.module.css";
+import insta from "../Assets/Instagram.JPG";
+import { Button, CardActions, Alert, TextField } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import bg from "../Assets/insta.png";
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+  Image,
+} from "pure-react-carousel";
+import CloudUploadIcon from "@material-ui/icons/CloudUpload";
+import "pure-react-carousel/dist/react-carousel.es.css";
+import { Link } from "react-router-dom";
+import img1 from "../Assets/img1.jpg";
+import img2 from "../Assets/img2.jpg";
+import img3 from "../Assets/img3.jpg";
+import img4 from "../Assets/img4.jpg";
+import img5 from "../Assets/img5.jpg";
+
+export default function Login() {
+  const useStyles = makeStyles({
+    text1: {
+      color: "grey",
+      textAlign: "center",
+    },
+    text2: {
+      textAlign: "center",
+    },
+    card2: {
+      height: "5vh",
+      marginTop: "2%",
+    },
+  });
+  const classes = useStyles();
+  return (
+    <div className={styles.loginWrapper}>
+      <div
+        className={styles.imgCar}
+        style={{ backgroundImage: `url(${bg})`, backgroundSize: "cover" }}
+      >
+        <div className={styles.car}>
+          <CarouselProvider
+            naturalSlideWidth={238}
+            naturalSlideHeight={423}
+            visibleSlides={1}
+            totalSlides={5}
+            hasMasterSpinner
+            isPlaying={true}
+            infinite={true}
+            dragonEnabled={true}
+            touchEnabled={true}
+          >
+            <Slider>
+              <Slide index={0}>
+                <Image src={img1}></Image>
+              </Slide>
+              <Slide index={1}>
+                <Image src={img2}></Image>
+              </Slide>
+              <Slide index={2}>
+                <Image src={img3}></Image>
+              </Slide>
+              <Slide index={3}>
+                <Image src={img4}></Image>
+              </Slide>
+              <Slide index={4}>
+                <Image src={img5}></Image>
+              </Slide>
+            </Slider>
+          </CarouselProvider>
+        </div>
+      </div>
+      <div className={styles.loginCard}>
+        <Card variant="outlined">
+          <div className={styles.instaLogo}>
+            <img src={insta} alt="Instagram" />
+          </div>
+          <CardContent>
+            {true && <Alert severity="error">Check it out</Alert>}
+            <TextField
+              id="outlined-basic"
+              label="Email"
+              variant="outlined"
+              fullWidth={true}
+              margin="dense"
+              size="small"
+            />
+            <TextField
+              id="outlined-basic"
+              label="Password"
+              variant="outlined"
+              fullWidth={true}
+              margin="dense"
+              size="small"
+            />
+            <Typography
+              className={classes.text2}
+              color="primary"
+              variant="subtitle1"
+            >
+              Forgot Password?
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button
+              size="small"
+              color="primary"
+              fullWidth={true}
+              variant="contained"
+            >
+              Log in
+            </Button>
+          </CardActions>
+        </Card>
+        <Card variant="outlined" className={classes.card2}>
+          <Typography className={classes.text1} variant="subtitle1">
+            Don't have an account ?{" "}
+            <Link to="/signup" style={{ textDecoration: "none" }}>
+              Sign up
+            </Link>
+          </Typography>
+        </Card>
+      </div>
+    </div>
+  );
+}
--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -4,39 +4,94 @@ import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import styles from "./Signup.module.css";
 import insta from "../Assets/Instagram.JPG";
-import { Button, CardActions } from "@mui/material";
+import { Button, CardActions, Alert, TextField } from "@mui/material";
 import { makeStyles } from "@mui/styles";
+import CloudUploadIcon from "@material-ui/icons/CloudUpload";
+import { Link } from "react-router-dom";
 
-export default function MultiActionAreaCard() {
+export default function Signup() {
   const useStyles = makeStyles({
-    text1:{
-      color: 'grey',
-      textAlign: 'centre'
-    }
-  })
+    text1: {
+      color: "grey",
+      textAlign: "center",
+    },
+    card2: {
+      height: "5vh",
+      marginTop: "2%",
+    },
+  });
   const classes = useStyles();
   return (
-    <div className= {styles.signupWrapper}>
-      <div className = {styles.signupCard}>
-      <Card variant="outlined">
-          <div className= {styles.instaLogo}>
+    <div className={styles.signupWrapper}>
+      <div className={styles.signupCard}>
+        <Card variant="outlined">
+          <div className={styles.instaLogo}>
             <img src={insta} alt="Instagram" />
           </div>
           <CardContent>
             <Typography variant="subtitle1" className={classes.text1}>
               Sign up to see photos and videos from friends.
             </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Lizards are a widespread group of squamate reptiles, with over
-              6,000 species, ranging across all continents except Antarctica
+            {true && <Alert severity="error">Check it out</Alert>}
+            <TextField
+              id="outlined-basic"
+              label="Email"
+              variant="outlined"
+              fullWidth={true}
+              margin="dense"
+              size="small"
+            />
+            <TextField
+              id="outlined-basic"
+              label="Password"
+              variant="outlined"
+              fullWidth={true}
+              margin="dense"
+              size="small"
+            />
+            <TextField
+              id="outlined-basic"
+              label="Full Name"
+              variant="outlined"
+              fullWidth={true}
+              margin="dense"
+              size="small"
+            />
+            <Button
+              size="small"
+              color="secondary"
+              fullWidth={true}
+              variant="outlined"
+              margin="dense"
+              startIcon={<CloudUploadIcon />}
+              component="label"
+            >
+              Upload Profile Image
+              <input type="file" accept="image/" hidden />
+            </Button>
+          </CardContent>
+          <CardActions>
+            <Button
+              size="small"
+              color="primary"
+              fullWidth={true}
+              variant="contained"
+            >
+              Share
+            </Button>
+          </CardActions>
+          <CardContent>
+            <Typography className={classes.text1} varaint="subtitle1">
+              By signing up, you agree to the Terms, Conditions and Cookies
+              policy.
             </Typography>
           </CardContent>
-        <CardActions>
-          <Button size="small" color="primary">
-            Share
-          </Button>
-        </CardActions>
-      </Card>
+        </Card>
+        <Card variant="outlined" className={classes.card2}>
+          <Typography className={classes.text1} variant="subtitle1">
+            Having an account ? <Link to="/login" style={{textDecoration : "none"}}>Login</Link>
+          </Typography>
+        </Card>
       </div>
     </div>
   );
